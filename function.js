40f9850acec280289610c946de22022b9b6b657d
SARAN911 <saranseliv518@gmail.com>
2025-11-03 23:33:01 +0530

    task1

---
+++ function.js
@@ -0,0 +1,15 @@
+const add=(num,num1)=>{
+return num+num1;
+}
+ const sub=(num,num1)=>{
+
+    return num-num1
+ }
+
+  const div=(num,num1)=>{
+
+    return num/num1
+ }
+
+ export default add;
+ export {sub,div};
---
+++ return.js
@@ -0,0 +1,4 @@
+import add,{sub,div} from "./function.js";
+console.log(add(1,2));
+console.log(sub(1,2));
+console.log(div(1,2));
