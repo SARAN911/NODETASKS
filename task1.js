bf5bad99de3964e9a2e2c6da5d601f33a135e0f5
SARAN911 <saranseliv518@gmail.com>
2025-11-05 03:13:33 +0530

    day2

---
+++ task1.js
@@ -0,0 +1,11 @@
+const add=(num1,num2)=>{
+return num1+num2
+}
+const sub=(num1,num2)=>{
+return num1-num2
+}
+const div=(num1,num2)=>{
+return num1/num2
+}
+export {sub,div};
+export default add;
---
+++ task1result.js
@@ -0,0 +1,4 @@
+import add,{sub,div} from "./task1.js";
+console.log(add(1,2));
+console.log(sub(1,2));
+console.log(div(1,2));
